647edcaf43df224ccd460b35f47bdcbb564905e5
Girish Baviskar <gbaviskar@umass.edu>
2025-04-19 17:42:14 -0400

    adds setter injection and qualifiers

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/girish/springcoredemo/common/Coach.java
@@ -0,0 +1,6 @@
+package com.girish.springcoredemo.common;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/girish/SpringCoreDemo/common/BaseballCoach.java
@@ -0,0 +1,13 @@
+package com.girish.SpringCoreDemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 mins in batting practice";
+    }
+
+}
